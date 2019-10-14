42c6afa33f74e99eb2e0c7cd68664713d0798247
woodtick88 <woodtick88@gmail.com>
2019-10-14 19:38:51 +0300

    routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import './app.css';
-import withBookstoreService from '../hoc';
+import { Route, Switch } from 'react-router-dom';
+import { HomePage, CartPage} from '../pages';
 
-const App = ({ bookstoreService }) => {
-    console.log(bookstoreService.getBooks())
-    return <div>App</div>
+import './app.css';
+
+const App = () => {
+    return (
+        <Switch>
+            <Route
+                path="/" 
+                component={HomePage}
+                exact
+            />
+            <Route
+                path="/cart" 
+                component={CartPage}
+            />
+        </Switch>
+    )
 };
 
-export default withBookstoreService()(App);
+export default App;
