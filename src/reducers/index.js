28f777b73ed8e21a3cb7dc0d631e4c9837bb6f5c
woodtick88 <woodtick88@gmail.com>
2019-10-16 20:51:46 +0300

    connecting a new component to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,25 +2,43 @@
 const initialState = {
     books: [],
     loading: true,
-    error: null
+    error: null,
+    cartItems: [
+        {
+            id: 1,
+            name: 'Book 1',
+            count: 3,
+            total: 350
+        },
+        {
+            id: 2,
+            name: 'Book 2',
+            count: 2,
+            total: 250
+        }
+    ],
+    orderTotal: 330
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'FETCH_BOOKS_REQUEST':
             return {
+                ...state,
                 books: [],
                 loading: true,
                 error: null
             };
         case 'FETCH_BOOKS_SUCCESS':
             return {
+                ...state,
                 books: action.payload,
                 loading: false,
                 error: null
             };
         case 'FETCH_BOOKS_FAILURE':
             return {
+                ...state,
                 books: [],                
                 loading: false,
                 error: action.payload
