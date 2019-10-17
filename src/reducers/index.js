aad8f86905baed1d0184f453b840a98a656120a0
woodtick88 <woodtick88@gmail.com>
2019-10-17 14:20:11 +0300

    redux update items in an array

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,57 +3,84 @@ const initialState = {
     books: [],
     loading: true,
     error: null,
-    cartItems: [
-    ],
-    orderTotal: 330
-};
-
-const reducer = (state = initialState, action) => {
-
-    switch (action.type) {
-        case 'FETCH_BOOKS_REQUEST':
-            return {
-                ...state,
-                books: [],
-                loading: true,
-                error: null
-            };
-        case 'FETCH_BOOKS_SUCCESS':
-            return {
-                ...state,
-                books: action.payload,
-                loading: false,
-                error: null
-            };
-        case 'FETCH_BOOKS_FAILURE':
-            return {
-                ...state,
-                books: [],                
-                loading: false,
-                error: action.payload
-            };
-        case 'BOOK_ADDED_TO_CART':
-            const bookId = action.payload;
-            const book = state.books.find((book) => book.id === bookId);
-            const newItem = {
-                id: book.id,
-                name: book.title,
-                count: 1,
-                total: book.price
-            }
-
-            return {
-                ...state,
-                cartItems: [
-                    ...state.cartItems,
-                    newItem
-                ]
-            };
-               
-        default:
-          return state;    
+    cartItems: [],
+    orderTotal: 220
+  };
+  
+  const updateCartItems = (cartItems, item, idx) => {
+  
+    if (idx === -1) {
+      return [
+        ...cartItems,
+        item
+      ];
     }
-    
-};
-
-export default reducer;
+  
+    return [
+      ...cartItems.slice(0, idx),
+      item,
+      ...cartItems.slice(idx + 1)
+    ];
+  };
+  
+  const updateCartItem = (book, item = {}) => {
+  
+    const {
+      id = book.id,
+      count = 0,
+      title = book.title,
+      total = 0 } = item;
+  
+    return {
+      id,
+      title,
+      count: count + 1,
+      total: total + book.price
+    };
+  };
+  
+  const reducer = (state = initialState, action) => {
+  
+    switch (action.type) {
+      case 'FETCH_BOOKS_REQUEST':
+        return {
+          ...state,
+          books: [],
+          loading: true,
+          error: null
+        };
+  
+      case 'FETCH_BOOKS_SUCCESS':
+        return {
+          ...state,
+          books: action.payload,
+          loading: false,
+          error: null
+        };
+  
+      case 'FETCH_BOOKS_FAILURE':
+        return {
+          ...state,
+          books: [],
+          loading: false,
+          error: action.payload
+        };
+  
+      case 'BOOK_ADDED_TO_CART':
+        const bookId = action.payload;
+        const book = state.books.find((book) => book.id === bookId);
+        const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
+        const item = state.cartItems[itemIndex];
+  
+        const newItem = updateCartItem(book, item);
+        return {
+          ...state,
+          cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
